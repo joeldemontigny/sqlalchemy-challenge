7b98afe4f117de60470ff00748b253897f1b39b0
joeldemontigny <joeldemontigny@yahoo.ca>
2023-06-23 11:31:39 -0400

    app start

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,11 +1,20 @@
 # Import the dependencies.
+import numpy as np
+import datetime as dt
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
 
 
 #################################################
 # Database Setup
 #################################################
 
+engine = create_engine("///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
 
